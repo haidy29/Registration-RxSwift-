f6dfe61d54e1d6932f551396eb68583ef13484a6
Haidy Saeed <haidysaeed33@gmail.com>
2025-01-26 12:17:23 +0200

    pod installation

---
+++ Registration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Registration
+//
+//  Created by Haidy Saeed on 26/01/2025.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
