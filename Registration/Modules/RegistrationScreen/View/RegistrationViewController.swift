d7fad52bda3356fd1a27738cf1f51f4432bc3389
Haidy Saeed <haidysaeed33@gmail.com>
2025-01-26 14:54:18 +0200

    Ubdate

--- Registration/Modules/RegistrationScreen/View/RegistrationViewController.swift
+++ Registration/Modules/RegistrationScreen/View/RegistrationViewController.swift
@@ -1,20 +1,29 @@
 //
-//  ViewController.swift
+//  RegistrationViewController.swift
 //  Registration
 //
 //  Created by Haidy Saeed on 26/01/2025.
 //
 
 import UIKit
-import RxSwift
 
 class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
